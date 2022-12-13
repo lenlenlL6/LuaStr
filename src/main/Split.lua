cc8dd9f9485476a297d7cf811b4dd4a6172e4db7
Karyl <62021442+lenlenlL6@users.noreply.github.com>
2022-12-13 21:29:23 +0700

    Update Split.lua

--- src/main/Split.lua
+++ src/main/Split.lua
@@ -33,19 +33,16 @@ function string:split(chars, maxSplit)
   if maxSplit < 0 and maxSplit ~= -1 then
     error("must be greater than -2")
   end
-  if self:sub(table.pack(self:find(chars))[1], table.pack(self:find(chars))[2]) ~= chars then
-    chars = ('%' .. chars)
-  end
   local result = {}
   local splitCount = 0
-  local startF, endF = self:find(chars)
+  local startF, endF = self:find(chars, 1, true)
   while startF do
     if splitCount >= maxSplit and maxSplit ~= -1 then
       break
     end
     table.insert(result, self:sub(1, (startF - 1)))
     self = self:sub((endF + 1))
-    startF, endF = self:find(chars)
+    startF, endF = self:find(chars, 1, true)
     splitCount = (splitCount + 1)
   end
   table.insert(result, self)
